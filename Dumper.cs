e3dde7f90ebc19042e6064d09a3167a33e067c2e
Lauro Moura <lauromoura@expertisesolutions.com.br>
2019-07-02 12:37:55 -0300

    Start dumping basic items

--- Dumper.cs
+++ Dumper.cs
@@ -2,15 +2,44 @@ using System;
 
 public class Dumper
 {
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
         {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Must provide a filename");
-                return;
-            }
-            ApiDump.Logging.Logger.AddConsoleLogger();
-
-            ApiDump.API.Parse(args[0]);
+            Console.WriteLine("Must provide a filename");
+            return;
         }
+        ApiDump.Logging.Logger.AddConsoleLogger();
+
+        var api = ApiDump.API.Parse(args[0]);
+
+        Dump("api.yaml", api);
+    }
+
+    public static void Dump(string filename, ApiDump.API api)
+    {
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter(filename))
+        {
+            file.WriteLine("--- # Classes");
+            foreach (var item in api.Classes)
+            {
+                file.WriteLine($"- {item.Name}");
+            }
+            file.WriteLine("--- # Enums");
+            foreach (var item in api.Enums)
+            {
+                file.WriteLine($"- {item.Name}");
+            }
+            file.WriteLine("--- # Structs");
+            foreach (var item in api.Structs)
+            {
+                file.WriteLine($"- {item.Name}");
+            }
+            file.WriteLine("--- # FunctionPointers");
+            foreach (var item in api.FunctionPointers)
+            {
+                file.WriteLine($"- {item.Name}");
+            }
+        }
+    }
 }
