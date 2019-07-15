1a681a7ddfc3c17e0971ed4192a3c202447a28db
Lauro Moura <lauromoura@expertisesolutions.com.br>
2019-07-15 19:31:11 -0300

    Experiment reading data with XMLReader

--- lib/Exml.Validator.cs
+++ lib/Exml.Validator.cs
@@ -2,28 +2,140 @@ using System;
 using System.Linq;
 using System.IO;
 using System.Xml;
+using System.Text;
 using System.Xml.Schema;
-using System.Xml.Linq;
+using System.Collections.Generic;
+
+using ApiDump.Logging;
 
 namespace Exml
 {
 namespace Validator
 {
 
-public class ExmlValidator
+// Internal representation of the EXML while we read it
+internal class Widget
 {
-    public static void Validate(Stream xml, Stream xsd, ValidationEventHandler handler)
-    {
-        XmlReader schemaReader = XmlReader.Create(xsd);
-        XmlSchemaSet schema = new XmlSchemaSet();
-        schema.Add(null, schemaReader);
+    public string Name { get; set; }
+    public Dictionary<string, string> Attributes { get; set; }
+    public List<Widget> Children { get; set; }
+    public Widget Parent { get; set; }
 
-        XmlReaderSettings settings = new XmlReaderSettings();
-        settings.Schemas = schema;
-        settings.ValidationType = ValidationType.Schema;
-        settings.ValidationEventHandler += handler;
-        XmlReader xmlReader = XmlReader.Create(xml, settings);
-        XDocument doc = XDocument.Load(xmlReader);
+    public Widget()
+    {
+        Attributes = new Dictionary<string, string>();
+        Children = new List<Widget>();
+    }
+
+    public override String ToString()
+    {
+        return ToString(0);
+    }
+
+    public string ToString(int indent)
+    {
+        var spaces = new String(' ', 4 * indent);
+        var sb = new StringBuilder();
+
+        sb.AppendLine(spaces + $"Widget: {Name}");
+
+        foreach (var entry in Attributes)
+        {
+            sb.AppendLine(spaces + $"    attrib: {entry.Key} => {entry.Value}");
+        }
+
+        if (Children.Count > 0)
+        {
+            foreach (var child in Children)
+            {
+                sb.AppendLine(spaces + child.ToString(indent + 1));
+            }
+        }
+
+        return sb.ToString();
+    }
+}
+
+public static class ExmlValidator
+{
+    public static void Validate(string path)
+    {
+        using (var file = File.OpenRead(path))
+        {
+            Validate(file);
+        }
+    }
+
+    public static void Validate(Stream stream)
+    {
+        // FIXME Maybe we should parametrize the settings
+        var settings = new XmlReaderSettings();
+        settings.ConformanceLevel = ConformanceLevel.Document;
+
+        using (var reader = XmlReader.Create(stream, settings))
+        {
+            Stack<Widget> stack = new Stack<Widget>();
+            Widget root = null;
+            Widget current = null;
+
+            while (reader.Read())
+            {
+
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Logger.Info($"Got element {reader.Name}");
+                        var parent = current;
+
+                        current = new Widget();
+                        current.Parent = parent;
+                        current.Name = reader.Name;
+                        // Do we need to remember before advancing to the attributes?
+                        bool isEmptyElement = reader.IsEmptyElement;
+
+                        if (reader.HasAttributes)
+                        {
+                            while (reader.MoveToNextAttribute())
+                            {
+                                Logger.Info($"Adding attribute {reader.Name} valued {reader.Value}");
+                                current.Attributes[reader.Name] = reader.Value;
+                            }
+                        }
+
+                        if (parent != null)
+                        {
+                            parent.Children.Add(current);
+                        }
+
+                        if (root == null)
+                        {
+                            root = current;
+                        }
+
+                        if (!reader.IsEmptyElement)
+                        {
+                            Logger.Info($"Pushing element {current.Name}");
+                            stack.Push(current);
+                        }
+                        else
+                        {
+                            Logger.Info($"Element {reader.Name} has no children. Not pushing...");
+                            current = parent;
+                        }
+
+                        break;
+                    case XmlNodeType.EndElement:
+                        current = stack.Pop();
+                        Logger.Info($"Popped element {current.Name}");
+                        break;
+                    default:
+                        Logger.Info($"Node {reader.NodeType} with value {reader.Value}");
+                        break;
+                }
+            }
+
+            Logger.Info($"Got tree: {root}");
+        }
     }
 }
 
--- test/Exml.Validator.Test.cs
+++ test/Exml.Validator.Test.cs
@@ -3,43 +3,25 @@ using System.Reflection;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
-
+using System.Xml;
 using System.Xml.Schema;
 
 using Exml.Validator;
-
 
 namespace TestSuite
 {
     public class Valid_Exml
     {
-        const string schema_name = "sample_schema.xsd";
-
         public static void valid(string test_folder)
         {
-            ValidationEventHandler cb = (object sender, ValidationEventArgs e) =>
-            {
-                Console.WriteLine($"{e.Severity}: {e.Message}");
-                Console.WriteLine($"{e.Exception.ToString()}");
-            };
-
-            string filePath = Path.Combine(test_folder, "hello_valid.xml");
-            string schemaPath = Path.Combine(test_folder, schema_name);
-            using (var xmlStream = File.OpenRead(filePath))
-            using (var schemaStream = File.OpenRead(schemaPath))
-            {
-                try
-                {
-                    ExmlValidator.Validate(xmlStream, schemaStream, cb);
-                }
-                catch (XmlSchemaException e)
-                {
-                    Console.WriteLine($"{e.ToString()}");
-                    throw e;
-                }
-            }
-
+            ExmlValidator.Validate(Path.Combine(test_folder, "hello_valid.xml"));
         }
+
+        public static void invalid(string test_folder)
+        {
+            Test.AssertRaises<XmlException>(() => ExmlValidator.Validate(Path.Combine(test_folder, "invalid_xml.xml")));
+        }
+
     }
 }
 
@@ -48,7 +30,7 @@ public class TestRunner
     static void Main(string[] args)
     {
         // FIXME control verbosity with `meson test -v`
-        /* ApiDump.Logging.Logger.AddConsoleLogger(); */
+        ApiDump.Logging.Logger.AddConsoleLogger();
         string test_folder = args[0];
         bool failed = false;
 
